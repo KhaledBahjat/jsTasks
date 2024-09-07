1e8223eed2084b625c82f311f3d4f8b5d3fe0775
Noston <khaledbahjat0@gmail.com>
2024-09-07 10:21:30 -0700

    new task

---
+++ day1.js
@@ -0,0 +1,19 @@
+var x = prompt()
+function studentUniversty(x) {
+  switch (x) {
+    case "  FCI":
+      alert("“You’re eligible to Programingtracks");
+      break;
+    case "Commerce":
+      alert("You’re eligible to ERPand Social media tracks");
+      break;
+    case "Engineering":
+      alert("You’re eligible toNetwork and Embedded tracks");
+      break;
+    default:
+      alert("You’re eligible to SW fundamentalstrack");
+      break;
+  }
+}
+
+studentUniversty(x);
